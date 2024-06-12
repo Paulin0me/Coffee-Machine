6b6a7753155f199d3cffc4990e5b89eb4ce9d9cd
Pauline <pauline.m.bonnet@gmail.com>
2024-06-12 12:39:45 +0200

    Refacto

--- src/DrinkMaker.ts
+++ src/DrinkMaker.ts
@@ -9,14 +9,12 @@ export class DrinkMaker {
   sugarAmount: number = 0;
 
   public makeDrink(drinkOrder: string) {
-    let drinkType = drinkOrder.charAt(0);
-    let sugarAmount = drinkOrder.charAt(2);
+    let [drinkType, sugarAmount] = drinkOrder.split(':', 2)
 
     if (drinkType === 'C') {
       this.drinkSelection = DrinkType.COFFEE
     }
-    
-    if (sugarAmount == '1') {
+    if (Number(sugarAmount) == 1) {
       this.sugarAmount = 1
     }
   }
