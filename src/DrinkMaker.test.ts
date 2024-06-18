3b359910d01af6747feddc2b36460549075a8b58
Pauline <pauline.m.bonnet@gmail.com>
2024-06-18 13:17:07 +0200

    Refacto tests

--- src/DrinkMaker.test.ts
+++ src/DrinkMaker.test.ts
@@ -3,24 +3,24 @@ import { DrinkMaker, DrinkType } from './DrinkMaker'
 
 describe('Drink Maker', () => {
   it('should make a coffee', () => {
-    assert(property(string(), (order) => {
+    assert(property(string(), () => {
       // Arrange
       const drinkMaker = new DrinkMaker()
 
       // Act
-      const answer = drinkMaker.makeDrink("C::1")
+      drinkMaker.makeDrink("C::1")
 
       // Assert
       expect(drinkMaker.drinkSelection).toBe(DrinkType.COFFEE)
     }))
   })
   it('should make a coffee with one sugar and a stick', () => {
-    assert(property(string(), (order) => {
+    assert(property(string(), () => {
       // Arrange
       const drinkMaker = new DrinkMaker()
 
       // Act
-      const answer = drinkMaker.makeDrink("C:1:1")
+      drinkMaker.makeDrink("C:1:1")
 
       // Assert
       expect(drinkMaker.drinkSelection).toBe(DrinkType.COFFEE)
@@ -28,12 +28,12 @@ describe('Drink Maker', () => {
     }))
   })
   it('should make a tea', () => {
-    assert(property(string(), (order) => {
+    assert(property(string(), () => {
       // Arrange
       const drinkMaker = new DrinkMaker()
 
       // Act
-      const answer = drinkMaker.makeDrink("T::1")
+      drinkMaker.makeDrink("T::1")
 
       // Assert
       expect(drinkMaker.drinkSelection).toBe(DrinkType.TEA)
@@ -41,24 +41,24 @@ describe('Drink Maker', () => {
   })
 
   it('should make a hot chocolate', () => {
-    assert(property(string(), (order) => {
+    assert(property(string(), () => {
       // Arrange
       const drinkMaker = new DrinkMaker()
 
       // Act
-      const answer = drinkMaker.makeDrink("H::1")
+      drinkMaker.makeDrink("H::1")
 
       // Assert
       expect(drinkMaker.drinkSelection).toBe(DrinkType.HOT_CHOCOLATE)
     }))
   })
   it('should make a hot chocolate with two sugars and a stick', () => {
-    assert(property(string(), (order) => {
+    assert(property(string(), () => {
       // Arrange
       const drinkMaker = new DrinkMaker()
 
       // Act
-      const answer = drinkMaker.makeDrink("H:2:1")
+      drinkMaker.makeDrink("H:2:1")
 
       // Assert
       expect(drinkMaker.drinkSelection).toBe(DrinkType.HOT_CHOCOLATE)
@@ -67,13 +67,13 @@ describe('Drink Maker', () => {
   })
 
   it('should send a message to the customer', () => {
-    assert(property(string(), (order) => {
+    assert(property(string(), () => {
       // Arrange
       const drinkMaker = new DrinkMaker()
       console.log = jest.fn();
 
       // Act
-      const answer = drinkMaker.makeDrink("M:message-content")
+      drinkMaker.makeDrink("M:message-content")
 
       // Assert
       expect(drinkMaker.drinkSelection).toBe(undefined)
@@ -82,12 +82,12 @@ describe('Drink Maker', () => {
   })
 
   it('should make a coffee if enough money is given', () => {
-    assert(property(string(), (order) => {
+    assert(property(string(), () => {
       // Arrange
       const drinkMaker = new DrinkMaker()
 
       // Act
-      const answer = drinkMaker.makeDrink("C:0:1")
+      drinkMaker.makeDrink("C:0:1")
 
       // Assert
       expect(drinkMaker.drinkSelection).toBe(DrinkType.COFFEE)
@@ -95,13 +95,13 @@ describe('Drink Maker', () => {
   })
 
   it('should not make a drink if enough money is given', () => {
-    assert(property(string(), (order) => {
+    assert(property(string(), () => {
       // Arrange
       const drinkMaker = new DrinkMaker()
       console.log = jest.fn();
 
       // Act
-      const answer = drinkMaker.makeDrink("C:0:0.1")
+      drinkMaker.makeDrink("C:0:0.1")
 
       // Assert
       expect(drinkMaker.drinkSelection).toBe(undefined)
@@ -110,12 +110,12 @@ describe('Drink Maker', () => {
   })
   
   it('should make a tea if enough money is given', () => {
-    assert(property(string(), (order) => {
+    assert(property(string(), () => {
       // Arrange
       const drinkMaker = new DrinkMaker()
 
       // Act
-      const answer = drinkMaker.makeDrink("T:0:0.4")
+      drinkMaker.makeDrink("T:0:0.4")
 
       // Assert
       expect(drinkMaker.drinkSelection).toBe(DrinkType.TEA)
@@ -123,12 +123,12 @@ describe('Drink Maker', () => {
   })
 
   it('should make a tea if enough money is given', () => {
-    assert(property(string(), (order) => {
+    assert(property(string(), () => {
       // Arrange
       const drinkMaker = new DrinkMaker()
 
       // Act
-      const answer = drinkMaker.makeDrink("H:0:0.5")
+      drinkMaker.makeDrink("H:0:0.5")
 
       // Assert
       expect(drinkMaker.drinkSelection).toBe(DrinkType.HOT_CHOCOLATE)
